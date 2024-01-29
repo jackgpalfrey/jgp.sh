fa989783bb767b14d68438323e7cf0ee141e0c9b
Jack Palfrey <jackgpalfrey@outlook.com>
2024-01-29 15:28:47 +0000

    feat: setup base webserver with mux

--- src/main.go
+++ src/main.go
@@ -1,25 +1,72 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "JGP.SH")
+}
+
+type HealthcheckReport struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+	Status  string `json:"status"`
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	log.Print("HEALTHCHECK")
+	w.Header().Set("Content-Type", "application/json")
+	report := HealthcheckReport{
+		Name: "JGP.SH",
+		Version: "indev",
+		Status: "OK",
+	}
+
+	err := json.NewEncoder(w).Encode(&report)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func redirect(w http.ResponseWriter, r *http.Request){
+	http.Redirect(w,r,"http://google.com", http.StatusPermanentRedirect)
+}
+
+type CreateShortURLResponse struct {
+	Short string `json:"short"`
+	Long string `json:"long"`
+}
+
+func create(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+
+	response := CreateShortURLResponse{
+		Short: "unknown",
+		Long: "unknown",
+	}
+
+	json.NewEncoder(w).Encode(&response)
+	
+}
 
 func main() {
 	fmt.Printf("Hello, world")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "JGP.SH")
-	})
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	router.HandleFunc("/vitals", healthcheck)
+	router.HandleFunc("/create", create).Methods("POST")
+	router.HandleFunc("/{code}", redirect)
 
-	log.Println("Creating tcp listener on port 8080")
-	listener, err := net.Listen("tcp", ":8080")
+	log.Printf("Listening on port 8080\n")
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	log.Println("Serving server on listener")
-	http.Serve(listener, nil)
 }
